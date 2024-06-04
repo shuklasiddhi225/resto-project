d24dbe2cdc929e237ead201ad3a159cb96dd1469
Siddhi Shukla <shuklasiddhi225@gmail.com>
2024-06-04 17:04:54 +0530

    initial commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,284 @@
+<!-- <template>
+    <div>
+        <div>
+            <img class="logo" src="../assets/Happy Indian (1).jpg"/>
+            <h1>Sign Up</h1>
+        </div>
+        <div class="register">
+            <input type="text" placeholder="Enter name"/> 
+            <input type="text" placeholder="Enter Email"/> 
+            <input type="password" placeholder="Enter Password"/> 
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.logo {
+    width: 100px;
+}
+
+.register input {
+    /* Retained original dimensions */
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    /* Retained original dimensions */
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style> -->
+<!-- <template>
+    <div class="container">
+        <div>
+            <img class="logo" src="../assets/Happy Indian (1).jpg"/>
+            <h1>Sign Up</h1>
+        </div>
+        <div class="register">
+            <input type="text" placeholder="Enter name" class="input-field"/> 
+            <input type="text" placeholder="Enter Email" class="input-field"/> 
+            <input type="password" placeholder="Enter Password" class="input-field"/> 
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.logo {
+    width: 100px;
+}
+
+.register {
+    text-align: center;
+}
+
+.input-field {
+    /* Increase size of placeholders */
+    width: 300px;
+    height: 40px;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    /* Retained original dimensions */
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>  --> 
+<!-- <template>
+    <div class="container">
+        <div>
+            <img class="logo" src="../assets/Happy Indian (1).jpg"/>
+            <h1>Sign Up</h1>
+        </div>
+        <div class="register">
+            <input type="text" placeholder="Enter name" class="input-field"/> 
+            <input type="text" placeholder="Enter Email" class="input-field"/> 
+            <input type="password" placeholder="Enter Password" class="input-field"/> 
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.logo {
+    width: 100px;
+}
+
+.register {
+    text-align: center;
+}
+
+.input-field {
+    /* Increase size of placeholders */
+    width: 300px;
+    height: 40px;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid skyblue;
+    display: block; /* Display input fields as block elements */
+}
+
+.register button {
+    /* Retained original dimensions */
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
+  -->
+  <!-- <template>
+    <div class="container">
+        <div>
+            <img class="logo" src="../assets/Happy Indian (1).jpg"/>
+            <h1>Sign Up</h1>
+        </div>
+        <div class="register">
+            <input type="text" placeholder="Enter name" class="input-field"/> 
+            <input type="text" placeholder="Enter Email" class="input-field"/> 
+            <input type="password" placeholder="Enter Password" class="input-field"/> 
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    
+}
+
+
+.logo {
+    width: 200px; /* Increased width */
+    height: auto; /* Maintain aspect ratio */
+}
+
+.register {
+    text-align: center;
+    margin-top: 20px;
+}
+
+.input-field {
+    /* Increase size of placeholders */
+    width: 300px;
+    height: 40px;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid skyblue;
+    display: block; /* Display input fields as block elements */
+}
+
+.register button {
+    /* Retained original dimensions */
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
+ -->
+ <template>
+    <div class="container">
+        <div>
+            <img class="logo" src="../assets/Happy Indian (1).jpg"/>
+            <h1 class="heading">Sign Up</h1> <!-- Added class "heading" -->
+        </div>
+        <div class="register">
+            <input type="text" placeholder="Enter name" class="input-field"/> 
+            <input type="text" placeholder="Enter Email" class="input-field"/> 
+            <input type="password" placeholder="Enter Password" class="input-field"/> 
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.logo {
+    width: 200px; /* Increased width */
+    height: auto; /* Maintain aspect ratio */
+    margin-bottom: -30px; /* Decreased gap between logo and heading */
+}
+
+.heading {
+    margin-bottom: 10px; /* Decreased gap between logo and heading */
+    text-align: center;
+}
+
+.register {
+    text-align: center;
+    margin-top: 10px;
+    margin-left: 20px; /* Retained gap between heading and register section */
+}
+
+.input-field {
+    /* Increase size of placeholders */
+    width: 300px;
+    height: 40px;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #DF8426;
+    display: block; /* Display input fields as block elements */
+}
+
+.register button {
+    /* Retained original dimensions */
+    width: 320px;
+    height: 40px;
+    border: 1px solid #DF8426;
+    background: #DF8426;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
+
